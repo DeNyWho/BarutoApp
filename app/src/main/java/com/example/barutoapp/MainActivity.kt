ab4898a5c0a12008f6e23fc7aeaffbb760ae5f8b
Ахунов Денис <denis.akhunov00@mail.ru>
2022-01-10 21:13:30 +0500

    Add Compose Navigation

--- app/src/main/java/com/example/barutoapp/MainActivity.kt
+++ app/src/main/java/com/example/barutoapp/MainActivity.kt
@@ -8,13 +8,23 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.barutoapp.navigation.SetupNavGraph
 import com.example.barutoapp.ui.theme.BarutoAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BarutoAppTheme {
+
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
 
             }
         }
